f6acabb5815e4b59c8580d6266fe03c114b47661
abelardo <ivo_v97@hotmail.com>
2018-10-16 13:58:42 -0500

    1 commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/yadiraeloisadavilacorona/Desktop/Trabajo/Dotdotdata/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    }]
